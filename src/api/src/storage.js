80cf783701a702f99e26aa44f8146836135aa84b
Dennis Skoko <dennis.skoko@hotmail.com>
2023-09-08 13:50:52 +0000

    Fix TypeScript error

--- src/api/src/storage.js
+++ src/api/src/storage.js
@@ -4,7 +4,7 @@ export async function read() {
   try {
     return JSON.parse(await readFile(process.env.LUNCH_STORAGE_FILE, 'utf8'))
   } catch (err) {
-    if (err.code === 'ENOENT') {
+    if (/** @type {NodeJS.ErrnoException} */ (err).code === 'ENOENT') {
       return []
     }
 

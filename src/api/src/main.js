f164e92f1ac2a38cc0e51425f2f7a8e876ed2313
Dennis Skoko <dennis.skoko@hotmail.com>
2023-09-08 13:51:15 +0000

    Remove index and just have main

--- src/api/src/main.js
+++ src/api/src/main.js
@@ -3,21 +3,19 @@ import helmet from 'helmet'
 import cors from 'cors'
 import * as storage from './storage.js'
 
-export function main() {
-  const app = express()
+const app = express()
 
-  app.use(helmet())
-  app.use(cors())
+app.use(helmet())
+app.use(cors())
 
-  app.get('/restaurants', async (_req, res, next) => {
-    try {
-      res.json(await storage.read())
-    } catch (err) {
-      next(err)
-    }
-  })
+app.get('/restaurants', async (_req, res, next) => {
+  try {
+    res.json(await storage.read())
+  } catch (err) {
+    next(err)
+  }
+})
 
-  app.listen(5000, () => {
-    console.log('Server started')
-  })
-}
+app.listen(5000, () => {
+  console.log('Server started')
+})

495d125c71a59a8c560fd3a6c1829405ac6eb86b
Dennis Skoko <dennis.skoko@hotmail.com>
2025-04-09 09:18:40 +0000

    Comment out Eatery since they have changed to PDF

--- src/api/src/restaurants/eatery.js
+++ src/api/src/restaurants/eatery.js
@@ -1,5 +1,5 @@
-import { JSDOM } from 'jsdom'
-import fetch from 'node-fetch'
+// import { JSDOM } from 'jsdom'
+// import fetch from 'node-fetch'
 
 export const name = 'Eatery'
 export const url = 'https://eatery.se/vastra-hamnen/lunchmeny'
@@ -8,70 +8,72 @@ export const url = 'https://eatery.se/vastra-hamnen/lunchmeny'
  * @returns {Promise<Course[]>}
  */
 export async function scrape() {
-  const response = await fetch('https://api.eatery.se/wp-json/eatery/v1/load')
+  // const response = await fetch('https://api.eatery.se/wp-json/eatery/v1/load')
 
-  if (!response.ok) {
-    throw new Error('Non ok response code from eatery')
-  }
+  // if (!response.ok) {
+  //   throw new Error('Non ok response code from eatery')
+  // }
 
-  /** @type {{ [key: string]: string }} */
-  const dayIndexToText = {
-    '1': 'MÅNDAG',
-    '2': 'TISDAG',
-    '3': 'ONSDAG',
-    '4': 'TORSDAG',
-    '5': 'FREDAG'
-  }
+  // /** @type {{ [key: string]: string }} */
+  // const dayIndexToText = {
+  //   '1': 'MÅNDAG',
+  //   '2': 'TISDAG',
+  //   '3': 'ONSDAG',
+  //   '4': 'TORSDAG',
+  //   '5': 'FREDAG'
+  // }
 
-  const dayAsText = dayIndexToText[new Date().getDay()]
+  // const dayAsText = dayIndexToText[new Date().getDay()]
 
-  const data = /** @type {any} */ (await response.json())
-  const menuId = data.eateries['/vastra-hamnen'].menues.lunchmeny
-  const dom = new JSDOM(data.menues[menuId].content.content)
-  const document = dom.window.document
+  // const data = /** @type {any} */ (await response.json())
+  // const menuId = data.eateries['/vastra-hamnen'].menues.lunchmeny
+  // const dom = new JSDOM(data.menues[menuId].content.content)
+  // const document = dom.window.document
 
-  /** @type {Element | null | undefined} */
-  let cursor = Array.from(document.querySelectorAll('h2, strong')).find(header => {
-    return header.textContent === dayAsText
-  })
+  // /** @type {Element | null | undefined} */
+  // let cursor = Array.from(document.querySelectorAll('h2, strong')).find(header => {
+  //   return header.textContent === dayAsText
+  // })
 
-  if (!cursor) throw new Error('Did not find header with todays day name')
+  // if (!cursor) throw new Error('Did not find header with todays day name')
 
-  if (cursor.parentElement?.lastElementChild === cursor) {
-    cursor = cursor.parentElement.nextElementSibling?.firstElementChild
-  } else {
-    cursor = cursor.nextElementSibling
-  }
+  // if (cursor.parentElement?.lastElementChild === cursor) {
+  //   cursor = cursor.parentElement.nextElementSibling?.firstElementChild
+  // } else {
+  //   cursor = cursor.nextElementSibling
+  // }
 
-  const courses = [
-    cursor?.textContent,
-    cursor?.nextElementSibling?.textContent,
-    cursor?.nextElementSibling?.nextElementSibling?.textContent
-  ]
+  // const courses = [
+  //   cursor?.textContent,
+  //   cursor?.nextElementSibling?.textContent,
+  //   cursor?.nextElementSibling?.nextElementSibling?.textContent
+  // ]
 
-  const weeklySalladCursor = Array.from(document.querySelectorAll('strong'))
-    .find(el => el.textContent?.toLowerCase().includes('veckans sallad'))
+  // const weeklySalladCursor = Array.from(document.querySelectorAll('strong'))
+  //   .find(el => el.textContent?.toLowerCase().includes('veckans sallad'))
 
-  if (weeklySalladCursor) {
-    // const weeklySallad = weeklySalladCursor.parentElement?.nextElementSibling?.textContent?.trim()
-    // if (weeklySallad) courses.push(weeklySallad)
-    // else {
-      let inlineWeeklySallad = weeklySalladCursor.parentElement?.textContent
-        ?.replace('Veckans sallad', '')
-        .replace(':', '')
-        .trim()
+  // if (weeklySalladCursor) {
+  //   // const weeklySallad = weeklySalladCursor.parentElement?.nextElementSibling?.textContent?.trim()
+  //   // if (weeklySallad) courses.push(weeklySallad)
+  //   // else {
+  //     let inlineWeeklySallad = weeklySalladCursor.parentElement?.textContent
+  //       ?.replace('Veckans sallad', '')
+  //       .replace(':', '')
+  //       .trim()
 
-      if (inlineWeeklySallad) courses.push(inlineWeeklySallad)
-    // }
-  }
+  //     if (inlineWeeklySallad) courses.push(inlineWeeklySallad)
+  //   // }
+  // }
 
-  return courses
-    .map(desc => desc?.trim())
-    .map((desc, i) => {
-      if (!desc) {
-        throw new Error('Scraped invalid course')
-      }
+  // return courses
+  //   .map(desc => desc?.trim())
+  //   .map((desc, i) => {
+  //     if (!desc) {
+  //       throw new Error('Scraped invalid course')
+  //     }
 
-      return { diet: i === 2 ? 'veg' : 'all', desc }
-    })
+  //     return { diet: i === 2 ? 'veg' : 'all', desc }
+  //   })
+
+  return []
 }

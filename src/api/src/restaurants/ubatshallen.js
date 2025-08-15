75d98f8d10b3297c7d2244bd62cad9fd935f64dc
Dennis Skoko <dennis.skoko@hotmail.com>
2025-08-15 12:49:47 +0000

    Update how Ubåtshallen is scraped

--- src/api/src/restaurants/ubatshallen.js
+++ src/api/src/restaurants/ubatshallen.js
@@ -21,15 +21,14 @@ export async function scrape() {
 
   const dayAsText = /** @type {string} */ (dayIndexToText[new Date().getDay()])
 
-  const header = Array.from(document.querySelectorAll('.entry-content .wp-block-group strong'))
+  const header = Array.from(document.querySelectorAll('.entry-content strong'))
     .find(header => header.textContent?.includes(dayAsText))
   if (!header) throw new Error('Could not find the header element')
 
-  const wrapper = header.closest('.wp-block-group')
-
+  const wrapper = header.parentElement?.nextElementSibling
   if (!wrapper) throw new Error('Could not find the wrapper element')
 
-  header.remove()
+  ;[...wrapper.querySelectorAll('strong')].forEach(element => element.replaceWith(document.createElement('br')))
   wrapper.innerHTML = wrapper.innerHTML.replaceAll('<br>', '\n')
   
   const courses = wrapper?.textContent
@@ -40,14 +39,14 @@ export async function scrape() {
 
   if (!courses) throw new Error('Failed to get the courses')
 
-  const weeklySalladHeader = Array.from(document.querySelectorAll('.entry-content .wp-block-group strong'))
+  const weeklySallad = Array.from(document.querySelectorAll('.entry-content p'))
     .find(header => header.textContent?.toLowerCase().includes("veckans sallad"))
-  if (!weeklySalladHeader) throw new Error('Could not find the weekly sallad element')
+  if (!weeklySallad) throw new Error('Could not find the weekly sallad element')
 
-  const weeklySallad = weeklySalladHeader.parentElement?.textContent?.split(':').pop()?.trim()
+  const weeklySalladText = weeklySallad.textContent?.split(':').pop()?.trim()
 
-  if (weeklySallad) {
-    courses?.push(weeklySallad)
+  if (weeklySalladText) {
+    courses?.push(weeklySalladText)
   }
 
   return courses.map((course, i) => ({ diet: i === 0 ? 'veg' : 'all', desc: course }))

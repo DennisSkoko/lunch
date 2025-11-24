ff6191c57f2d0c1961c86e692e518f558b478d53
Dennis Skoko <dennis.skoko@hotmail.com>
2025-11-24 09:33:31 +0000

    Update Varv again...

--- src/api/src/restaurants/varv.js
+++ src/api/src/restaurants/varv.js
@@ -21,34 +21,34 @@ export async function scrape() {
 
   const dayAsText = dayIndexToText[new Date().getDay()]
 
-  const header =[...document.querySelectorAll('h4')]
+  const header =[...document.querySelectorAll('h2')]
     .find(element => element.textContent?.trim().toLowerCase() === dayAsText)
   if (!header) throw new Error('Could not find the header for today')
 
-  // const firstMenuItem = header?.nextElementSibling
-  // if (!firstMenuItem) throw new Error('Could not find the first menu item element')
+  const firstMenuItem = header?.nextElementSibling
+  if (!firstMenuItem) throw new Error('Could not find the first menu item element')
 
-  // const secondMenuItem = header?.nextElementSibling?.nextElementSibling
-  // if (!secondMenuItem) throw new Error('Could not find the second menu item element')
+  const secondMenuItem = header?.nextElementSibling?.nextElementSibling?.nextElementSibling
+  if (!secondMenuItem) throw new Error('Could not find the second menu item element')
 
-  // return [firstMenuItem, secondMenuItem]
-  //   .map(el => el.textContent?.trim())
-  //   .filter(line => !!line)
-  //   .map((menuItem, i) => /** @type {Course} */ ({
-  //     diet: i === 1 ? 'veg' : 'all',
-  //     desc: menuItem
-  //   }))
-  
-  const menuItem = header?.nextElementSibling
-  if (!menuItem) throw new Error('Could not find the menu item element')
-
-  return menuItem?.innerHTML
-    .replaceAll('<br>', '\n')
-    .replaceAll('<em>or</em>', '\n')
-    .split('\n')
-    .filter(line => line.trim())
+  return [firstMenuItem, secondMenuItem]
+    .map(el => el.textContent?.trim())
+    .filter(line => !!line)
     .map((menuItem, i) => /** @type {Course} */ ({
       diet: i === 1 ? 'veg' : 'all',
       desc: menuItem
     }))
+  
+  // const menuItem = header?.nextElementSibling
+  // if (!menuItem) throw new Error('Could not find the menu item element')
+
+  // return menuItem?.innerHTML
+  //   .replaceAll('<br>', '\n')
+  //   .replaceAll('<em>or</em>', '\n')
+  //   .split('\n')
+  //   .filter(line => line.trim())
+  //   .map((menuItem, i) => /** @type {Course} */ ({
+  //     diet: i === 1 ? 'veg' : 'all',
+  //     desc: menuItem
+  //   }))
 }

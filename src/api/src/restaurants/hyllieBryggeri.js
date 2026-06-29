0fd1271c815eae69df5d781fea4730882fd94529
Dennis Skoko <dennis.skoko@hotmail.com>
2026-06-29 09:25:24 +0000

    Update Hyllie Bryggeri

--- src/api/src/restaurants/hyllieBryggeri.js
+++ src/api/src/restaurants/hyllieBryggeri.js
@@ -1,66 +1,37 @@
-import { loadJsdomFromUrl } from '../util.js'
-
 export const name = 'Hyllie Bryggeri'
-export const url = 'https://www.hylliebryggeri.se/meny'
+export const url = 'https://hylliebryggeri.se/stormgatan'
 export const emoji = 'beer'
 
 /**
  * @returns {Promise<Course[]>}
  */
 export async function scrape() {
-  const dom = await loadJsdomFromUrl(url)
-  const document = dom.window.document
+  const response = await fetch('https://xvfynakyjzmckotiykww.supabase.co/rest/v1/lunch_menus?select=week_number,monday,tuesday,wednesday,thursday,friday,vegetarian,fish,monday_en,tuesday_en,wednesday_en,thursday_en,friday_en,vegetarian_en,fish_en,monday_de,tuesday_de,wednesday_de,thursday_de,friday_de,vegetarian_de,fish_de,monday_da,tuesday_da,wednesday_da,thursday_da,friday_da,vegetarian_da,fish_da&location=eq.stormgatan', {
+    headers: {
+      'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2ZnluYWt5anptY2tvdGl5a3d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MzQ0MjEsImV4cCI6MjA5NjQxMDQyMX0.Yuc5npMGj5cTMZqawLOdJEawXDDgMD9A9xH5TcayfZw'
+    }
+  })
+
+  if (!response.ok) throw new Error('Request to Hyllie Bryggeri API failed')
+
+  const result = await response.json()
+  const data = result[0]
 
   /** @type {{ [key: string]: string }} */
   const dayIndexToText = {
-    1: 'Måndag',
-    2: 'Tisdag',
-    3: 'Onsdag',
-    4: 'Torsdag',
-    5: 'Fredag'
+    '1': 'monday',
+    '2': 'tuesday',
+    '3': 'wednesday',
+    '4': 'thursday',
+    '5': 'friday'
   }
 
-  const dayText = dayIndexToText[new Date().getDay()]
-  if (!dayText) throw new Error('No lunch menu on weekends.')
+  const dayAsText = /** @type {string} */ (dayIndexToText[new Date().getDay()])
+  const todaysMenu = data[dayAsText]
 
-  // Get all visible text on the page
-  const fullText = document.body.textContent
-    .replace(/\r/g, '')
-    .replace(/\n{2,}/g, '\n')
-    .trim()
-
-  /**
-   * Match:
-   *  - The current weekday
-   *  - Everything until the next weekday OR end of text
-   */
-  const dayRegex = new RegExp(
-    `${dayText}\\s*([\\s\\S]*?)(?=Måndag|Tisdag|Onsdag|Torsdag|Fredag|$)`,
-    'i'
-  )
-
-  const match = fullText.match(dayRegex)
-  if (!match || !match[1]) {
-    throw new Error(`Could not find menu for: ${dayText}`)
-  }
-
-  const menuBlock = match[1]
-  .split('\n')
-  .map(l =>
-    l
-      .trim()
-      // remove leading punctuation like ":" "–" "-"
-      .replace(/^[\s:–\-.,]+/, '')
-      // remove trailing punctuation
-      .replace(/[\s:–\-.,]+$/, '')
-  )
-  .filter(Boolean)
-
-
-  if (menuBlock.length === 0 || !menuBlock[0]) {
-    throw new Error(`Empty menu for: ${dayText}`)
-  }
-
-  // Take only 1st line - Swedish course line
-  return [{ diet: 'all', desc: menuBlock[0] }]
+  return [
+    { diet: 'all', desc: todaysMenu },
+    { diet: 'veg', desc: data.vegetarian }
+  ]
 }
+
